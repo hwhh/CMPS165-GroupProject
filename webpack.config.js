5ba3db173e97985d6995ba2948e1264b866fc6de
Henry Hargreaves <hwhh@st-andrews.ac.uk>
2018-06-06 15:22:21 -0700

    got webpack loader working

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+    entry: './scripts/index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+};
