cb6744a6859b1f4671a7dd84530b1fc6d5dab8bd
Hailey Pun <haileypun@eduroam-169-233-229-245.ucsc.edu>
2018-06-06 17:21:19 -0700

    Commit

--- scripts/main.js
+++ scripts/main.js
@@ -305,17 +305,17 @@ function createSlider(){
 createSlider();
 
 
-//slider.transition() // Gratuitous intro!
-//    .duration(750)
-//    .tween("hue", function() {
-//      var i = d3.interpolate(0, 70);
-//      return function(t) { hue(i(t)); };
-//    });
+slider.transition() // Gratuitous intro!
+    .duration(750)
+    .tween("hue", function() {
+      var i = d3.interpolate(0, 70);
+      return function(t) { hue(i(t)); };
+    });
 
-//function hue(h) {
-//  handle.attr("cx", x(h));
+function hue(h) {
+  handle.attr("cx", x(h));
 //  svg.style("background-color", d3.hsl(h, 0.8, 0.8));
-//}
+}
 
 
 //Map = Country -> {Year, Value}
