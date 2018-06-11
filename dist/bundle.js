8a575d3197428883d980f19ee04d91d6f77198cd
Henry Hargreaves <hwhh@st-andrews.ac.uk>
2018-06-10 21:08:36 -0700

    added the variables

--- dist/bundle.js
+++ dist/bundle.js
@@ -81,7 +81,7 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./scripts/line_chart.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = "./scripts/index.js");
 /******/ })
 /************************************************************************/
 /******/ ({
